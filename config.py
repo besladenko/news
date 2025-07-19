3399b70197e00b0e7303e25981da68846b81bdf3
Vladimir <besladen@gmail.com>
2025-07-19 15:44:11 +0300

    dsfsadfs

--- config.py
+++ config.py
@@ -11,11 +11,11 @@ class Config:
     Все настройки загружаются из переменных окружения.
     """
     # Настройки базы данных PostgreSQL
-    DB_USER = os.getenv("DB_USER", "user")
-    DB_PASSWORD = os.getenv("DB_PASSWORD", "password")
+    DB_USER = os.getenv("DB_USER", "sex")
+    DB_PASSWORD = os.getenv("DB_PASSWORD", "sex")
     DB_HOST = os.getenv("DB_HOST", "localhost")
     DB_PORT = os.getenv("DB_PORT", "5432")
-    DB_NAME = os.getenv("DB_NAME", "setinews_db")
+    DB_NAME = os.getenv("DB_NAME", "lohness")
     DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
     # Токены Telegram ботов
