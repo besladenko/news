1c45a2ea2a8a32ece0239e5e3232712b1c14a5bf
Vladimir <besladen@gmail.com>
2025-07-20 13:11:48 +0300

    аыаыфва

--- bots/admin_bot.py
+++ bots/admin_bot.py
@@ -4,8 +4,6 @@ from bots.handlers import city, donor, pending, publish
 
 bot = Bot(token=settings.ADMIN_BOT_TOKEN, parse_mode="HTML")
 dp = Dispatcher()
-
-# Для admin_bot все хендлеры (управление, модерация, publish)
 dp.include_router(city.router)
 dp.include_router(donor.router)
 dp.include_router(pending.router)
--- bots/news_bot.py
+++ bots/news_bot.py
@@ -1,13 +1,6 @@
 from aiogram import Bot, Dispatcher
-from aiogram.types import Message
 from config.settings import settings
-from bots.handlers import city, donor, pending, publish
 
 bot = Bot(token=settings.NEWS_BOT_TOKEN, parse_mode="HTML")
 dp = Dispatcher()
-
-# Подключаем нужные хендлеры (у news_bot только публикация, pending)
-dp.include_router(city.router)
-dp.include_router(donor.router)
-dp.include_router(pending.router)
-dp.include_router(publish.router)
+# Не подключаем routers!
